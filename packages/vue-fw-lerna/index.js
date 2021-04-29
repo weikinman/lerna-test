3aeb3aa0c05d9f313c546c669745745329a79cb5
scott <scott@boardware.com>
2021-04-29 14:42:27 +0800

    test

---
+++ packages/vue-fw-lerna/index.js
@@ -0,0 +1,27 @@
+import utils from "@vue-fw-lerna/utils";
+
+let components = [];
+
+let plugins = [];
+
+const install = (app, opt) => {
+    const option = Object.assign(defaultInstallOpt, opt)
+    app.config.globalProperties.$ELEMENT = option;
+
+    components.forEach(component => {
+        app.component(component.name, component)
+    });
+
+    plugins.forEach(plugin => {
+        app.use(plugin)
+    });
+}
+
+export {
+    utils,
+    install
+}
+
+export default {
+    install
+}
