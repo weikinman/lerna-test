819db231b27676307df8706c5eca3a906ce199a4
scott <scott@boardware.com>
2021-04-29 14:34:50 +0800

    test

--- packages/vue-fw-lerna/index.js
+++ packages/vue-fw-lerna/index.js
@@ -6,7 +6,7 @@ let plugins = [];
 
 const install = (app, opt) => {
     const option = Object.assign(defaultInstallOpt, opt)
-    app.config.globalProperties.$ELEMENT = option
+    app.config.globalProperties.$ELEMENT = option;
 
     components.forEach(component => {
         app.component(component.name, component)
